3b18f31f80750e0b2377ae49c9b886d2a21d8e66
atul <atkr5496@gmail.com>
2025-12-31 14:13:16 +0530

    brand theme

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from "storybook/theming";
+
+export default create({
+  base: "dark",
+  brandTitle: "Mario",
+  brandUrl: "https://github.com/en-atul/mario",
+  //   brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
+  brandTarget: "_self",
+});
